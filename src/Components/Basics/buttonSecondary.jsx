da0acacc31051395d8b993a8b5ee3db038fead50
Anouar-91 <a.berrouane@hotmail.com>
2023-07-19 16:41:58 +0200

    create component myButton

---
+++ src/Components/Basics/buttonSecondary.jsx
@@ -0,0 +1,29 @@
+import { Button } from 'native-base';
+import React from 'react'
+import { StyleSheet } from 'react-native';
+import colors from '../../constants/colors';
+
+const MyButton= (props) => {
+    const {
+        children,
+        onClick,
+        color = "primary"
+    } = props;
+
+    return (
+        <Button onPress={onClick} style={color == "primary" ? styles.primary : styles.secondary}>{children}</Button>
+    )
+}
+
+export default MyButton
+
+const styles = StyleSheet.create({
+    primary: {
+        backgroundColor: colors.primary,
+        borderRadius: 20
+    },
+    secondary: {
+        backgroundColor: colors.secondary,
+        borderRadius: 20
+    }
+})
